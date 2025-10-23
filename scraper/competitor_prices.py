f9a83a833d1c556e026d58774e389fa7301b6516
injetisrilatha-rgb <injeti.srilatha@gmail.com>
2025-10-23 13:16:47 +0200

    Create competitor_prices.py

---
+++ scraper/competitor_prices.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_prices(product_name):
+    # Simulated scraping logic
+    competitors = {
+        "Alza": 899,
+        "Heureka": 950,
+        "Mall.cz": 920
+    }
+    return competitors
